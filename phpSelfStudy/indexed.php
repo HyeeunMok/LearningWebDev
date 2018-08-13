fb97f09ea833e6b8d631b6f3853ee589fbbcfd76
Hyeeun Mok <41407354+HyeeunMok@users.noreply.github.com>
2018-08-12 21:53:51 -0400

    Add files via upload

---
+++ phpSelfStudy/indexed.php
@@ -0,0 +1,11 @@
+<?php
+// $characters = array("Arthur Dent", "Ford Prefect", "Zaphod Beedblebrox");
+// echo $characters;
+
+$characters = ["Arthur Dent", "Ford Prefect", "Zaphod Beedblebrox"];
+
+$characters[] = "Marvin";
+$characters[] = "Slartibartfast"; 
+// print_r($characters);
+
+echo $characters[1];
