358cdbafc1ad80f2e537f1a942f4dec9b7feddf5
Hyeeun Mok <41407354+HyeeunMok@users.noreply.github.com>
2018-08-13 21:07:03 -0400

    Add files via upload

---
+++ phpSelfStudy/display_values.php
@@ -0,0 +1,21 @@
+<?php
+$firstName = "David";
+$lastName = "Powers";
+$number = 4200.99;
+
+/* $1Name = "David";
+ $first-Name = "David";
+*/
+
+echo $number, " ", $firstName;
+//print $number;
+
+echo "<br/>";
+
+echo $firstName;
+echo " ";
+echo $lastName;
+
+echo "<br/>";
+
+echo $firstName . " " . $lastName;
